48666e1d1314e73a64f661caa36ea763c2b21788
Prarthana Ganegama <105261574+GKPrarthana@users.noreply.github.com>
2025-07-29 17:10:29 +0530

    clerk public key added

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { ClerkProvider } from '@clerk/clerk-react'
 import App from './App.tsx'
 import './index.css'
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.pk_test_Z2l2aW5nLXRvYWQtMzAuY2xlcmsuYWNjb3VudHMuZGV2JA
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Clerk Publishable Key")
